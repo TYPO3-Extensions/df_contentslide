6124782dfb84139567e111eeb4c9f31810c79bd9
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-26 14:52:21 +0000

    [TASK] Release of version 3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/df_contentslide/trunk@53457 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "df_contentslide".
 #
-# Auto generated 10-02-2011 22:39
+# Auto generated 26-10-2011 16:51
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Collapsing and expanding of content elements (http://mootools.net/demos/?demo=Fx.Slide) with dynamic or static loading.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '2.1.0',
+	'version' => '3.0.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:19:"de.locallang_db.xml";s:4:"113c";s:12:"ext_icon.gif";s:4:"3b38";s:14:"ext_tables.php";s:4:"5380";s:14:"ext_tables.sql";s:4:"dbaf";s:16:"locallang_db.xml";s:4:"33cf";s:27:"configuration/constants.txt";s:4:"fc78";s:23:"configuration/setup.txt";s:4:"53e9";s:30:"resources/css/df_contentslide.css";s:4:"48fc";s:36:"resources/javascript/df_contentslide.js";s:4:"abdd";}',
+	'_md5_values_when_last_written' => 'a:14:{s:21:"class.loadcontent.php";s:4:"79bd";s:19:"de.locallang_db.xml";s:4:"f08e";s:12:"ext_icon.gif";s:4:"3b38";s:17:"ext_localconf.php";s:4:"dc7b";s:14:"ext_tables.php";s:4:"e244";s:14:"ext_tables.sql";s:4:"2ac8";s:16:"locallang_db.xml";s:4:"451b";s:27:"configuration/constants.txt";s:4:"55c4";s:23:"configuration/setup.txt";s:4:"fe39";s:14:"doc/manual.sxw";s:4:"f85d";s:33:"resources/css/df_contentslide.css";s:4:"b689";s:32:"resources/images/ajax-loader.gif";s:4:"5335";s:25:"resources/images/icon.png";s:4:"5d0d";s:39:"resources/javascript/df_contentslide.js";s:4:"b219";}',
 	'suggests' => array(
 	),
 );
