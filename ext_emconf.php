204a87e402813857534cc156ed36f1c06aa16a75
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-05-30 12:32:01 +0000

    [TASK] Increase compatibility constraints (TYPO3 6.2 and PHP 5.5)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/df_contentslide/trunk@85438 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,17 +34,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-5.4.99',
-			'typo3' => '4.7.0-6.1.99',
+			'php' => '5.3.0-5.5.99',
+			'typo3' => '4.7.0-6.2.99',
 		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
+		'conflicts' => array(),
+		'suggests' => array(),
 	),
 	'_md5_values_when_last_written' => 'a:17:{s:21:"class.loadcontent.php";s:4:"295c";s:19:"de.locallang_db.xml";s:4:"c733";s:21:"ext_conf_template.txt";s:4:"9cfc";s:12:"ext_icon.gif";s:4:"3b38";s:17:"ext_localconf.php";s:4:"d0ef";s:14:"ext_tables.php";s:4:"7a18";s:14:"ext_tables.sql";s:4:"2ac8";s:16:"locallang_db.xml";s:4:"0557";s:27:"configuration/constants.txt";s:4:"1dff";s:23:"configuration/setup.txt";s:4:"9c03";s:39:"configuration/bodyTagCssClass/setup.txt";s:4:"501c";s:14:"doc/manual.sxw";s:4:"5f61";s:33:"resources/css/df_contentslide.css";s:4:"a171";s:32:"resources/images/ajax-loader.gif";s:4:"5335";s:25:"resources/images/icon.png";s:4:"5d0d";s:39:"resources/javascript/df_contentslide.js";s:4:"4663";s:43:"resources/javascript/jquery.contentslide.js";s:4:"77e4";}',
-	'suggests' => array(
-	),
+	'suggests' => array(),
 );
 
 ?>
